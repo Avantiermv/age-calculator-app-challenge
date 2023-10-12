9bc7899d33633323f3d577e303460e57b6f76964
marcos vinicius <avantiermv@gmail.com>
2023-10-12 15:33:00 -0300

    .

--- logic.js
+++ logic.js
@@ -1,3 +1,18 @@
-let day = querySelector('input#iday')
-let mounth = querySelector('input#imounth')
-let year = querySelector('input#iyear')
+function calcular() {
+    let dano = new Date()
+    let ano = dano.getFullYear()
+    let dias = new Date()
+    let dia = dias.getDay()
+    let dmês = new Date()
+    let mês = dmês.getMonth()
+
+    let day = document.querySelector('input#iday')
+    let month = document.querySelector('input#imonth')
+    let year = document.querySelector('input#iyear')
+    
+    if (day.value.length == 0 || day.value > dia) {
+        window.alert('ERRO')
+    } else if (month.value.length == 0 || month.value > month) {
+        window.alert('ERRO2')
+    }
+}
