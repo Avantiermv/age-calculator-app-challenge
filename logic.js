f1cddfd180af99d533adca89c3630c9e69dfdd30
marcos vinicius <avantiermv@gmail.com>
2024-01-01 23:41:41 -0300

    Avemaria cheia de graça!

--- logic.js
+++ logic.js
@@ -1,18 +1,7 @@
-function calcular() {
-    let dano = new Date()
-    let ano = dano.getFullYear()
-    let dias = new Date()
-    let dia = dias.getDay()
-    let dmês = new Date()
-    let mês = dmês.getMonth()
 
-    let day = document.querySelector('input#iday')
-    let month = document.querySelector('input#imonth')
-    let year = document.querySelector('input#iyear')
-    
-    if (day.value.length == 0 || day.value > dia) {
-        window.alert('ERRO')
-    } else if (month.value.length == 0 || month.value > month) {
-        window.alert('ERRO2')
-    }
+window.onload = function(){
+    const day = document.getElementById("iday")
+    const month = document.getElementById("imonth")
+    const year = document.getElementById("iyear")
+    const labels = 
 }
